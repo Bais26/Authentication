8161f6f25b5887d58c9984c0c27b839be7794e45
Bais26 <baisyufan2004@gmail.com>
2024-08-31 00:20:05 +0700

    change home by adding a function to greet the user

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
-  return (
-    <div>
-      hallo ini home ya makasih sudah login
-    </div>
-  )
+    // Ambil nama pengguna dari localStorage
+    const username = localStorage.getItem('username') || 'Pengguna';
+
+    return (
+        <section className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+            <h1 className="text-2xl font-bold mb-4">Selamat Datang, {username}!</h1>
+            <p className="text-lg">Ini adalah halaman utama Anda.</p>
+        </section>
+    );
 }
 
-export default Home
+export default Home;
